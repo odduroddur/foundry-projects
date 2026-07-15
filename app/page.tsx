e41c1505e1f0b8332e3d3a64fd060da318250185
odduroddur <oddur1506@gmail.com>
2026-07-15 12:37:10 +0200

    Add app/page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+import Header from '@/components/Header'
+import Hero from '@/components/Hero'
+import ProjectGrid from '@/components/ProjectGrid'
+import Footer from '@/components/Footer'
+
+export default function Home() {
+  return (
+    <main>
+      <Header />
+      <Hero />
+      <ProjectGrid />
+      <Footer />
+    </main>
+  )
+}
