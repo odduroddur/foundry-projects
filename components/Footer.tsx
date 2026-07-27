956bb0d80b009acc7983d3d0fd1d805ee3ea6b52
odduroddur <oddur1506@gmail.com>
2026-07-27 22:39:04 +0200

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -14,17 +14,12 @@ export default function Footer() {
           </div>
           <div className="text-center">
             <h3 className="text-xl font-bold mb-4">Contact</h3>
-            <a 
-              href="mailto:oddur1506@gmail.com" 
-              className="text-gray-300 hover:text-primary transition block mb-2"
-            >
-              Email
-            </a>
+            <p className="text-gray-300 mb-2">oddur1506@gmail.com</p>
             <a 
               href="https://www.linkedin.com/in/oddur-m%C3%A1r-oddsson-90a1a1265/" 
               target="_blank" 
               rel="noopener noreferrer" 
-              className="text-gray-300 hover:text-primary transition block"
+              className="text-gray-300 hover:text-primary transition"
             >
               LinkedIn
             </a>
